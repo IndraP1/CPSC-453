c658d95e246e150b7855a17c5e7f9eb524951649
mdpraset <mdpraset@gfx03-sb.cs.ucalgary.ca>
2016-10-13 17:48:33 -0600

    WIP sobel filter

--- Assignment2/fragment.glsl
+++ Assignment2/fragment.glsl
@@ -17,6 +17,40 @@ uniform sampler2DRect tex;
 uniform vec3 luminance;
 uniform float sepia;
 
+float intensity(in vec4 pixel) 
+{
+	return sqrt((pixel.x*pixel.x)+(pixel.y*pixel.y)+(pixel.z*pixel.z));
+}
+
+float threshold(in float thr1, in float thr2 , in float val) {
+	if (val < thr1) {return 0.0;}
+	if (val > thr2) {return 1.0;}
+	return val;
+}
+
+vec3 detect_edge(vec2 coords)
+{
+	float d = 1.0/512.0;
+
+	float tleft = intensity(texture2DRect(tex,vec2(-d,d)));
+	float cleft = intensity(texture2DRect(tex,vec2(-d,0)));
+	float bleft = intensity(texture2DRect(tex,vec2(-d,-d)));
+	float tcenter = intensity(texture2DRect(tex,vec2(0,d)));
+	float bcenter = intensity(texture2DRect(tex,vec2(0,-d)));
+	float tright = intensity(texture2DRect(tex,vec2(d,d)));
+	float cright = intensity(texture2DRect(tex,vec2(d,0)));
+	float bright = intensity(texture2DRect(tex,vec2(d,-d)));
+
+	float x = tleft + 2.0*cleft + bleft - tright - 2.0*cright - bright;
+	float y = -tleft - 2.0*tcenter - tright + bleft + 2.0*bcenter + bright;
+	
+	float color = sqrt((x*x) + (y*y));
+	if (color > 1.0)
+		return vec3(0.0,0.0,0.0);
+
+	return vec3(1.0,1.0,1.0);
+}
+
 void main(void)
 {
     int res = 1;
@@ -43,6 +77,8 @@ void main(void)
 		colour.g = (inputRed * .349) + (inputGreen *.686) + (inputBlue * .168);
 		colour.b = (inputRed * .272) + (inputGreen *.534) + (inputBlue * .131);
 	}
+
+	colour.rgb = detect_edge(textureCoords.xy);
 
     FragmentColour = colour;
 }
