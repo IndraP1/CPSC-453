259a6cdee018ab6f3e22b6099028f5d1cd7ec48e
mdpraset <mdpraset@zone38-eb.cs.ucalgary.ca>
2016-11-19 16:53:03 -0700

    WIP shading

--- Assignment4/boilerplate.cpp
+++ Assignment4/boilerplate.cpp
@@ -211,43 +211,304 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-bool intersectSphere(const vec3& c, float r, float x, float y, float z) {
-	/* ddt^2 + 2d(o-c) + (o-c)*(o-c) -R^2 = 0 */
-	vec3 o(0, 0, 0);	
-	vec3 d(x, y, z);
+struct Plane {
+	vec3 p;
+	vec3 n;
+
+	vec3 intersect;
+	float intmag;
+};
+
+struct Light {
+	vec3 p;
+	vec3 r;
+	float intensity;
+};
+
+struct Sphere {
+	vec3 c;
+	float r;
+
+	vec3 n;
+	vec3 intersect;
+	float intmag;
+};
+
+struct Triangle {
+	Plane pl;
+	vec3 p0;
+	vec3 p1;
+	vec3 p2;
+	vec3 px;
+
+	float a;
+	float a0;
+	float a1;
+	float a2;
+
+	float u;
+	float v;
+	float w;
+
+	vec3 intersect;
+	float intmag;
+};
+
+bool intersectSphere(Sphere& cr, vec3& d, vec3& o) {
 	float A = dot(d, d);
-	float B = 2*dot(d, (o-c));
-	float C = (dot((o-c), (o-c))-pow(r,2));
+	float B = 2*dot(d, (o-cr.c));
+	float C = (dot((o-cr.c), (o-cr.c))-pow(cr.r,2));
 	
-	float poly = pow(B,2)-(4*A*C);
-	if (poly < 0) {
+	float quad = pow(B,2)-(4*A*C);
+	if (quad < 0) {
 		return false;
 	}
+	float t = (-B + sqrt(quad))/(2*A);
+	cr.intersect = o + t*d;
+	cr.n = cr.intersect = cr.c;
+	cr.intmag = length(cr.intersect);
+	
 	return true;
 }
 
-void renderSphere() {
-	coord3D ray;
-	vec3 center(0.9, -1.925, -6.69);
-	float radius = 0.825;
+void initializeTrianglePlane(Triangle& t) {
+	vec3 u(t.p1 - t.p0);
+	vec3 v(t.p2 - t.p0);
 
-	ray.z = -577.35; // tan(120)*1024
+	t.pl.n.x = dot(u.y, v.z)-dot(u.z, v.y);
+	t.pl.n.y = dot(u.z, v.x)-dot(u.x, v.z);
+	t.pl.n.z = dot(u.x, v.y)-dot(u.y, v.x);
+	t.pl.p = t.p0;
+}
+
+float intersectPlane(Plane& pl, vec3& d, vec3& o) {
+	float t1 = dot((pl.p-o), pl.n);
+	float t2 = dot(d, pl.n);
+
+	if (t2 == 0)
+		return 0;
+
+	pl.intersect = o + (t1/t2)*d;
+	pl.intmag = length(pl.intersect);
+	return t1/t2;
+}
+
+void initializeTriangle(Triangle& t) {
+	vec3 p01(t.p1-t.p0);
+	vec3 p02(t.p2-t.p0);
+	vec3 p0x(t.px-t.p0);
+	vec3 p12(t.p2-t.p1);
+	vec3 p1x(t.px-t.p1);
+	vec3 p20(t.p0-t.p2);
+	vec3 p2x(t.px-t.p2);
+	
+	vec3 tmp(cross(p01, p02));
+	t.a = tmp.x+tmp.y+tmp.z;
+
+	vec3 tmp2(cross(p12, p1x));
+	t.a0 = tmp2.x+tmp2.y+tmp2.z;
+
+	vec3 tmp3(cross(p20, p2x));
+	t.a1 = tmp3.x+tmp3.y+tmp3.z;
+
+	vec3 tmp4(cross(p01, p0x));
+	t.a2 = tmp4.x+tmp4.y+tmp4.z;
+
+	t.v = t.a2/t.a;
+	t.u = t.a1/t.a;
+	t.w = t.a0/t.a;
+}
+
+
+bool intersectTriangle(Triangle& tr, vec3&d, vec3& o) {
+	initializeTrianglePlane(tr);
+	float t = intersectPlane(tr.pl, d, o);
+	if (t == 0)
+		return false;
+	tr.px = {(o.x + t*d.x), (o.y + t*d.y), (o.z + t*d.z)};
+	initializeTriangle(tr);
+	if (tr.u*tr.v < 0 || tr.u*tr.w < 0 || tr.v*tr.w < 0)
+		return false;
+
+	tr.intersect = o + t*d; 
+	tr.intmag = length(tr.intersect);
+	return true;
+}
+
+void renderShapes() {
+	coord3D ray;
+	vec3 origin(0, 0, 0);
+
+	Sphere cr;
+	cr.c = {0.9, -1.925, -6.69};
+	cr.r = 0.825;
+
+	// Light 
+	Light l;
+	l.p = {0, 2.5, -7.75};
+
+	// Plane 
+	Plane pl;
+	pl.p = {0, 0, -10.5};
+	pl.n = {0, 0, 1};
+
+	// Blue triangle
+	Triangle bt1;
+	bt1.p0 = {-0.4, -2.75, -9.55};
+	bt1.p1 = {-0.93, 0.55, -8.51};
+	bt1.p2 = {0.11, -2.75, -7.98};
+
+	Triangle bt2;
+	bt2.p0 = { 0.11, -2.75, -7.98};
+	bt2.p1 = {-0.93, 0.55, -8.51};
+	bt2.p2 = {-1.46, -2.75, -7.47};
+
+	Triangle bt3;
+	bt3.p0 = {-1.46, -2.75, -7.47};
+	bt3.p1 = {-0.93, 0.55, -8.51};
+	bt3.p2 = {-1.97, -2.75, -9.04};
+
+	Triangle bt4;
+	bt4.p0 = {-1.97, -2.75, -9.04};
+	bt4.p1 = {-0.93, 0.55, -8.51};
+	bt4.p2 = {-0.4, -2.75, -9.55};
+	Triangle bluepyramid[4] = {bt1, bt2, bt3, bt4};
+
+	// Ceiling
+	Triangle c1;
+	c1.p0 = { 2.75, 2.75, -10.5};
+	c1.p1 = { 2.75, 2.75, -5};
+	c1.p2 = { -2.75, 2.75, -5};
+
+	Triangle c2;
+	c2.p0 = { -2.75, 2.75, -10.5};
+	c2.p1 = { 2.75, 2.75, -10.5};
+	c2.p2 = { -2.75, 2.75, -5};
+	Triangle ceiling[2] = {c1, c2};
+
+	/* Green wall on right */ 
+	Triangle gw1;
+	gw1.p0 = {2.75, 2.75, -5};
+	gw1.p1 = {2.75, 2.75, -10.5};
+	gw1.p2 = {2.75, -2.75, -10.5};
+
+	Triangle gw2;
+	gw2.p0 = {2.75, -2.75, -5};
+	gw2.p1 = {2.75, 2.75, -5};
+	gw2.p2 = {2.75, -2.75, -10.5};
+	Triangle greenwall[2] = {gw1, gw2};
+
+	/* Red wall on left */
+	Triangle rw1;
+	rw1.p0 = {-2.75, -2.75, -5};
+	rw1.p1 = {-2.75, -2.75, -10.5};
+	rw1.p2 = {-2.75, 2.75, -10.5};
+
+	Triangle rw2;
+	rw2.p0 = {-2.75, 2.75, -5};
+	rw2.p1 = {-2.75, -2.75, -5};
+	rw2.p2 = {-2.75, 2.75, -10.5};
+	Triangle redwall[2] = {rw1, rw2};
+
+	/* Floor */
+	Triangle f1;
+	f1.p0 = {2.75, -2.75, -5};
+	f1.p1 = {2.75, -2.75, -10.5};
+	f1.p2 = {-2.75, -2.75, -10.5};
+
+	Triangle f2;
+	f2.p0 = {-2.75, -2.75, -5};
+	f2.p1 = {2.75, -2.75, -5};
+	f2.p2 = {-2.75, -2.75, -10.5};
+
+	Triangle floor[2] = {f1, f2};
+	ray.z = -950;// tan(120)*1024
 
 	for (int x = 0; x < img.Width(); x++) {
 		for (int y = 0; y < img.Height(); y++) {
+			float closest_mag = 200;
 			ray.x = -1*(img.Width()/2.f - 0.5f)+x;
 			ray.y = -1*(img.Height()/2.f - 0.5f)+y;
 			vec3 colour(0, 0, 0);
+			vec3 d(ray.x, ray.y, ray.z);
 
-			if (intersectSphere(center, radius, ray.x, ray.y, ray.z)) {
-				colour.x = 1;
-				colour.y = 1;
-				colour.z = 1;
+			if (intersectPlane(pl, d, origin) > 0.f) {
+				if(pl.intmag < closest_mag) {
+					closest_mag = pl.intmag;
+					l.intensity = dot(normalize(pl.n), (normalize(l.p-pl.intersect)));
+					colour.x = 1*l.intensity;
+					colour.y = 1*l.intensity;
+					colour.z = 1*l.intensity;
+				}
 			}
-			/* vec3 rayNorm(ray.x, ray.y, ray.z); */
-			/* rayNorm = normalize(rayNorm); */
+			for (uint i = 0; i < sizeof(bluepyramid)/sizeof(Triangle); i++) {
+				if (intersectTriangle(bluepyramid[i], d, origin) > 0.f) {
+					if(bluepyramid[i].intmag < closest_mag) {
+						closest_mag = bluepyramid[i].intmag;
+						l.intensity = dot(normalize(bluepyramid[i].pl.n), (normalize(l.p-bluepyramid[i].intersect)));
+						colour.x = 0;
+						colour.y = 0;
+						colour.z = 1*l.intensity;
+					}
+				}
+			}
+			for (uint i = 0; i < sizeof(floor)/sizeof(Triangle); i++) {
+				if (intersectTriangle(floor[i], d, origin) > 0.f) {
+					if(floor[i].intmag < closest_mag) {
+						closest_mag = floor[i].intmag;
+						l.intensity = dot(normalize(floor[i].pl.n), (normalize(l.p-floor[i].intersect)));
+						colour.x = 0.3*l.intensity;
+						colour.y = 0.3*l.intensity;
+						colour.z = 0.3*l.intensity;
+					}
+				}
+			}
 
-			/* vec3 colour(rayNorm.x, rayNorm.y, rayNorm.z); */
+			for (uint i = 0; i < sizeof(ceiling)/sizeof(Triangle); i++) {
+				if (intersectTriangle(ceiling[i], d, origin) > 0.f) {
+					if(ceiling[i].intmag < closest_mag) {
+						closest_mag = ceiling[i].intmag;
+						l.intensity = dot(normalize(ceiling[i].pl.n), (normalize(l.p-ceiling[i].intersect)));
+						colour.x = 0.3*l.intensity;
+						colour.y = 0.3*l.intensity;
+						colour.z = 0.3*l.intensity;
+					}
+				}
+			}
+
+			for (uint i = 0; i < sizeof(greenwall)/sizeof(Triangle); i++) {
+				if (intersectTriangle(greenwall[i], d, origin)) {
+					if(greenwall[i].intmag < closest_mag) {
+						closest_mag = greenwall[i].intmag;
+						l.intensity = dot(normalize(greenwall[i].pl.n), (normalize(l.p-greenwall[i].intersect)));
+						colour.x = 0;
+						colour.y = 1*l.intensity;
+						colour.z = 0;
+					}
+				}
+			}
+
+			for (uint i = 0; i < sizeof(redwall)/sizeof(Triangle); i++) {
+				if (intersectTriangle(redwall[i], d, origin) > 0.f) {
+					if(redwall[i].intmag < closest_mag) {
+						closest_mag = redwall[i].intmag;
+						l.intensity = dot(normalize(redwall[i].pl.n), (normalize(l.p-redwall[i].intersect)));
+						colour.x = 1*l.intensity;
+						colour.y = 0;
+						colour.z = 0;
+					}
+				}
+			}
+			if (intersectSphere(cr, d, origin)) {
+				if(cr.intmag < closest_mag) {
+					closest_mag = cr.intmag;
+					l.intensity = dot(normalize(cr.n), (normalize(l.p-cr.intersect)));
+					colour.x = 0.5*l.intensity;
+					colour.y = 0.5*l.intensity;
+					colour.z = 0.5*l.intensity;
+					cout << l.intensity << endl;
+				}
+			}
 			img.SetPixel(x, y, colour);
 		}
 	}
@@ -314,24 +575,7 @@ int main(int argc, char *argv[])
 	MyGeometry geometry;
 	if (!InitializeGeometry(&geometry))
 		cout << "Program failed to intialize geometry!" << endl;
-	renderSphere();
-	/* coord3D ray; */
-	/* coord3D origin = {0,0,0}; */
-
-	/* for (int x = 0; x < img.Width(); x++) { */
-	/* 	for (int y = 0; y < img.Height(); y++) { */
-	/* 		ray.x = float(x) / float(img.Width()); */
-	/* 		ray.x *= 2; */
-	/* 		ray.x -= 1; */
-	/* 		ray.y = float(y) / float(img.Width()); */
-	/* 		ray.y *= 2; */
-	/* 		ray.y -= 1; */
-	/* 		ray.z = 1; */
-
-	/* 		vec3 colour(abs(ray.x), abs(ray.y), ray.z); */
-	/* 		img.SetPixel(x, y, colour); */
-	/* 	} */
-	/* } */
+	renderShapes();
 	// run an event-triggered main loop
 	while (!glfwWindowShouldClose(window))
 	{
